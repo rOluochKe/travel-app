b985f3a1c1cdb6a365be1c3143161fcb9d27a834
Raymond Oluoch <rasholuoch@gmail.com>
2024-02-26 18:58:49 +0300

    setup splash screen navigation

--- travel_app/App.js
+++ travel_app/App.js
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as Splashscreen from 'expo-splash-screen';
+import { useCallback } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Header } from 'react-native/Libraries/NewAppScreen';
+import { Onboarding } from './screens';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    regular: require('./assets/fonts/regular.otf'),
+    medium: require('./assets/fonts/medium.otf'),
+    bold: require('./assets/fonts/bold.otf'),
+    light: require('./assets/fonts/light.otf'),
+    xtrabold: require('./assets/fonts/xtrabold.otf'),
+  })
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await Splashscreen.hideAsync();
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name='Onboard'
+          component={Onboarding}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ travel_app/screens/onboarding/Onboarding.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import styles from './onboarding.style'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const Onboarding = () => {
+  return (
+    <SafeAreaView>
+      <Text>Onboarding</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Onboarding
